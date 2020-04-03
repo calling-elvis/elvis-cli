efc218b1feddb82a53698d4c3e3777b3c5e7cfdc
clearloop <udtrokia@gmail.com>
2020-04-04 04:53:49 +0800

    update: make create-elvis-app and elvis-cli standalone

--- mode.sh
+++ mode.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-readonly index_path='\.\.\/\.\.\/\.\.\/elvis\/web\/pkg';
-readonly src_path='\.\.\/\.\.\/\.\.\/\.\.\/elvis\/web\/pkg';
+readonly INDEX_PATH='\.\.\/elvis\/web\/pkg';
+readonly SRC_PATH='\.\.\/\.\.\/elvis\/web\/pkg';
 
 if [[ -e .dev.lock ]]; then
     rm .dev.lock
-    cd ./packages/calling-elvis
-    sed -i '.bak' "s/${index_path}/elvis-web/g" index.ts
-    sed -i '.bak' "s/${src_path}/elvis-web/g" src/*.ts
+    sed -i '.bak' "s/${INDEX_PATH}/elvis-web/g" index.ts
+    sed -i '.bak' "s/${SRC_PATH}/elvis-web/g" src/*.ts
     rm *.bak src/*.bak
     echo '[npm]: convert calling-elvis into npm mode'
 else
     touch .dev.lock
-    cd ./packages/calling-elvis
-    sed -i '.bak' "s/elvis-web/${index_path}/g" index.ts
-    sed -i '.bak' "s/elvis-web/${src_path}/g" src/*.ts
+    sed -i '.bak' "s/elvis-web/${INDEX_PATH}/g" index.ts
+    sed -i '.bak' "s/elvis-web/${SRC_PATH}/g" src/*.ts
     rm *.bak src/*.bak
     echo '[dev]: convert calling-elvis into development mode'
 fi
